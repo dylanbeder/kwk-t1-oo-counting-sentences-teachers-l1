277215ec90b60cef057b602ac5d2f25bbd7c11e1
dylanbeder <dylan.beder@duke.edu>
2018-06-16 22:00:15 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
-
-  def question?
-
+    if self.end_with?(".") ==true
+      return true
+    end
+    return false
   end
 
   def exclamation?
+if self.end_with?("!") ==true
+      return true
+    end
+    return false
+  end
 
+  def question?
+if self.end_with?("?") ==true
+      return true
+    end
+    return false
   end
 
   def count_sentences
-
+  str=self.split
+  str.each do |item|
+    if !(item==" ")
+      count+=1
+    end
+  end
+  return count
   end
 end
